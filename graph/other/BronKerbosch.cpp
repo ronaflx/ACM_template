c01e1f8fd134a74816d5e7fb0d896e02be2a2c7c
Lixin Fan <900831flx@gmail.com>
2012-08-31 20:45:18 +0800

    update gauss ellmination and some graph theory template

--- graph/other/BronKerbosch.cpp
+++ graph/other/BronKerbosch.cpp
@@ -2,71 +2,43 @@
  * 极大团就一个被不被其他的完全子图包含的完全子图
  * 最大团一定是一个极大团，但是极大团不一定是最大团
 */
-class Bron_Kerbosch
+const static int N = 130;
+int n, maps[N][N], cnt;
+void countClique(int *p, int ps, int *x, int xs)
 {
-private:
-	const static int N = 130;
-	int n, maps[N][N], cnt;
-	void countClique(int *p, int ps, int *x, int xs)
-	{
-		if(ps == 0)
-		{
-			if(xs == 0)
-				cnt++;
-			return ;
-		}
-		for(int i = 0;i < xs;i++)
-		{
-			int j, v = x[i];
-			for(j = 0;j < ps && maps[p[j]][v];j++);
-			if(j == ps)
-				return;
-		}
-		int tmpp[N], tmpps = 0, tmpx[N], tmpxs = 0;
-		for(int i = 0;i < ps;i++)
-		{
-			int v = p[i];
-			tmpps = tmpxs = 0;
-			for(int j = i + 1;j < ps;j++)
-			{
-				int u = p[j];
-				if(maps[v][u])
-					tmpp[tmpps++] = u;
-			}
-			for(int j = 0;j < xs;j++)
-			{
-				int u = x[j];
-				if(maps[v][u])
-					tmpx[tmpxs++] = u;
-			}
-			countClique(tmpp, tmpps, tmpx, tmpxs);
-			if(cnt > 1000)
-				return;
-			x[xs++] = v;
-		}
-	}
-public:
-	void initialize(int n,int m)
-	{
-		memset(maps, 0, sizeof(maps));
-		this->n = n;
-		for(int i = 0;i < m;i++)
-		{
-			int a, b;
-			scanf("%d %d", &a, &b);
-			a--, b--;
-			maps[a][b] = true;
-			maps[b][a] = true;
-		}
-	}
-	int countClique()
-	{
-		cnt = 0;
-		int  p[N], x[N];
-		for(int i = 0;i < n;i++)
-			p[i] = i;
-		countClique(p, n, x, 0);
-		return cnt;
-	}
-}one ;
+        if(ps == 0) {
+                if(xs == 0) cnt++;
+                return ;
+        }
+        for(int i = 0;i < xs;i++) {
+                int j, v = x[i];
+                for(j = 0;j < ps && maps[p[j]][v];j++);
+                if(j == ps) return;
+        }
+        int tmpp[N], tmpps = 0, tmpx[N], tmpxs = 0;
+        for(int i = 0;i < ps;i++) {
+                int v = p[i];
+                tmpps = tmpxs = 0;
+                for(int j = i + 1;j < ps;j++) {
+                        int u = p[j];
+                        if(maps[v][u])
+                                tmpp[tmpps++] = u;
+                }
+                for(int j = 0;j < xs;j++) {
+                        int u = x[j];
+                        if(maps[v][u])
+                                tmpx[tmpxs++] = u;
+                }
+                countClique(tmpp, tmpps, tmpx, tmpxs);
+                if(cnt > 1000) return;
+                x[xs++] = v;
+        }
+}
+int countClique() {
+        cnt = 0;
+        int  p[N], x[N];
+        for(int i = 0;i < n;i++) p[i] = i;
+        countClique(p, n, x, 0);
+        return cnt;
+}
 
