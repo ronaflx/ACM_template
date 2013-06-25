9d485e3d2e4f4881b264fc4e103413e953f89eb8
Lixin Fan <900831flx@gmail.com>
2013-06-25 15:09:54 +0800

    update SplayTree

--- dataStructure/Splay.cpp
+++ dataStructure/Splay.cpp
@@ -1,6 +1,5 @@
 struct node {
-/* virtual node if tot is equal to 0*/
-#define __JUDGE if(tot == 0) return;
+#define __JUDGE if(tot == 0) return
 	static const int INF = 100000000;
 	node* ch[2], *pre;
 	int v, minn, tot, delta,  flip;
@@ -9,11 +8,11 @@ struct node {
 		ch[0] = l, ch[1] = r;
 	}
 	inline int min_v() { return minn; }
-	inline int size() { return tot; }
-	void reverse() { __JUDGE flip ^= 1; }
-	void add(int d) { __JUDGE minn += d, delta += d, v += d; }
+	inline int size()  { return  tot; }
+	void reverse() { __JUDGE; flip ^= 1; }
+	void add(int d) { __JUDGE; minn += d, delta += d, v += d; }
 	void push_down() {
-		__JUDGE
+		__JUDGE;
 		if(delta) {
 			if(ch[0]->tot) ch[0]->add(delta);
 			if(ch[1]->tot) ch[1]->add(delta);
@@ -26,7 +25,7 @@ struct node {
 		flip = delta = 0;
 	}
 	void push_up() {
-		__JUDGE
+		__JUDGE;
 		tot = ch[0]->size() + ch[1]->size() + 1;
 		minn = min(v, min(ch[0]->min_v(), ch[1]->min_v()));
 	}
@@ -34,17 +33,15 @@ struct node {
 class splay_tree {
 public:
 	splay_tree() {
-		null = new node(node::INF, 0, 0, 0, 0);
-		root = null;
+		root = null = new node(node::INF, 0, 0, 0, 0);
 	}
 	~splay_tree() {
 		clear(root);
 		delete null;
 	}
-	// make a sequence from 1 to n do build(0, n + 1, val)
-	// and make sure val[0] = va[1] = INF;
+	// build(0, n + 1, val) make a sequence from 1 to n
 	void build(int l, int r, int* val) {
-		if(l > r) return;
+		if(l > r) return;// and make sure val[0] = va[1] = INF;
 		build(l, r, root, null, val);
 	}
 #define centre (root->ch[1]->ch[0])
@@ -63,26 +60,23 @@ public:
 		centre->reverse();
 		splay(centre, null);
 	}
-	void revolve(int a, int b, int c) { // c < b - a + 1
+	void revolve(int a, int b, int c) {// c < b - a + 1
 		if(c == 0) return;
 		int len = b - a + 1;
 		reverse(a, a + len - c - 1);
-		reverse(a + len - c, b);
-		reverse(a, b);
+		reverse(a + len - c, b), reverse(a, b);
 	}
 	void insert(int a, int c) {
 		makeInterval(a + 1, a);
 		centre = new node(c, 1, null, null, root->ch[1]);
-		root->ch[1]->push_up();
-		root->push_up();
+		root->ch[1]->push_up(), root->push_up();
 		splay(centre, null);
 	}
 	void erase(int a) {
 		makeInterval(a, a);
 		delete centre;
 		centre = null;
-		root->ch[1]->push_up();
-		root->ch[0]->push_up();
+		root->ch[1]->push_up(), root->ch[0]->push_up();
 	}
 #undef centre
 	void clear() { clear(root); }
@@ -90,8 +84,7 @@ private:
 	node* root, * null;
 	void clear(node*& now) {
 		if(now == null) return;
-		clear(now->ch[0]);
-		clear(now->ch[1]);
+		clear(now->ch[0]), clear(now->ch[1]);
 		delete now;
 		now = null;
 	}
@@ -100,7 +93,7 @@ private:
 		node *y = x->pre;
 		y->push_down(), x->push_down();
 		y->ch[!type] = x->ch[type];
-		if (x->ch[type] != null) 
+		if (x->ch[type] != null)
 			x->ch[type]->pre = y;
 		x->pre = y->pre;
 		if (y->pre != null) {
@@ -110,16 +103,16 @@ private:
 				y->pre->ch[0] = x;
 		}
 		x->ch[type] = y, y->pre = x;
-		if (y == root) root = x; 	
+		if (y == root) root = x;
 		y->push_up(), x->push_up();
 	}
 	void splay(node* x, node* f) {
 		x->push_down();
 		while(x->pre != f) {
 			if (x->pre->pre == f) {
-				if (x->pre->ch[0] == x) 
+				if (x->pre->ch[0] == x)
 					rotate(x, 1);
-				else 
+				else
 					rotate(x, 0);
 			} else {
 				node *y = x->pre;
@@ -164,44 +157,6 @@ private:
 		splay(now, pre);
 	}
 	void makeInterval(int a, int b) {
-		findK(a - 1, null);
-		findK(b + 1, root);
+		findK(a - 1, null), findK(b + 1, root);
 	}
 }tree;
-const int N = 300000;
-int val[N], n, m, a, b, c;
-int main() {
-	char cmd[100];
-	while(scanf("%d", &n) == 1) {
-		for(int i = 1;i <= n;i++) scanf("%d", &val[i]);
-		val[0] = val[n + 1] = node::INF;
-		tree.clear();
-		tree.build(0, n + 1, val);
-		scanf("%d", &m);
-		REP(i, 0, m) {
-			scanf("%s", cmd);
-			if(!strcmp(cmd, "ADD")) {
-				scanf("%d %d %d", &a, &b, &c);
-				tree.add_value(a, b, c);
-			} else if(!strcmp(cmd, "REVERSE")) {
-				scanf("%d %d", &a, &b);
-				tree.reverse(a, b);
-			} else if(!strcmp(cmd, "REVOLVE")) {
-				scanf("%d %d %d", &a, &b, &c);
-				int tot = b - a + 1;
-				c = (c % tot + tot) % tot;
-				tree.revolve(a, b, c);
-			} else if(!strcmp(cmd, "INSERT")) {
-				scanf("%d %d", &a, &c);
-				tree.insert(a, c);
-			} else if(!strcmp(cmd, "DELETE")) {
-				scanf("%d", &a);
-				tree.erase(a);
-			} else if(!strcmp(cmd, "MIN")) {
-				scanf("%d %d", &a, &b);
-				printf("%d\n", tree.min_value(a, b));
-			}
-		}
-	}
-	return 0;
-}
