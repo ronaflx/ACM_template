837ed76cdec6082de93e6511b022972f3b868819
Lixin Fan <900831flx@gmail.com>
2014-01-02 13:27:03 +0800

    code format

--- graph/spanningTree/MDT(pool).cpp
+++ graph/spanningTree/MDT(pool).cpp
@@ -1,57 +1,55 @@
 const int N = 1010, E = N * N;
 const LL INF = 10000000000LL;
-template<typename T>
+template <typename T>
 struct Edge {
-    int u, v;
-    T c;
+  int u, v;
+  T c;
 };
 Edge<LL> edge[E];
 int label[N], pre[N], visit[N];
-template<typename T>
+template <typename T>
 T treeGraph(int n, int m, int root, Edge<T>* edge) {
-    int cnt = 0;
-    T inEdge[N], ans = 0;
-    while(true) {
-        fill(inEdge, inEdge + n, INF);
-        REP(i, 0, m) {
-            int u = edge[i].u, v = edge[i].v;
-            if(v != u && edge[i].c < inEdge[v])
-            {
-                pre[v] = u;
-                inEdge[v] = edge[i].c;
-            }
-        }
-        REP(i, 0, n) {
-            if(i == root) continue;
-            if(inEdge[i] == INF) return -1;
-        }
-        int now = 0;
-        CC(label, -1), CC(visit, -1);
-        inEdge[root] = 0;
-        REP(i, 0, n) {
-            ans += inEdge[i];
-            int v = i;
-            while(visit[v] != i && label[v] == -1 && v != root) {
-                visit[v] = i;
-                v = pre[v];
-            }
-            if(v != root && label[v] == -1) {
-                for(int u = pre[v]; u != v; u = pre[u])
-                    label[u] = now;
-                label[v] = now++;
-            }
-        }
-        if(now == 0) break;
-        REP(i, 0, n) if(label[i] == -1) label[i] = now++;
-        REP(i, 0, m) {
-            int v = edge[i].v;
-            edge[i].v = label[edge[i].v];
-            edge[i].u = label[edge[i].u];
-            if(edge[i].v != edge[i].u) edge[i].c -= inEdge[v];
-        }
-        root = label[root];
-        n = now;
+  int cnt = 0;
+  T inEdge[N], ans = 0;
+  while (true) {
+    fill(inEdge, inEdge + n, INF);
+    REP(i, 0, m) {
+      int u = edge[i].u, v = edge[i].v;
+      if (v != u && edge[i].c < inEdge[v]) {
+        pre[v] = u;
+        inEdge[v] = edge[i].c;
+      }
     }
-    return ans;
+    REP(i, 0, n) {
+      if (i == root) continue;
+      if (inEdge[i] == INF) return -1;
+    }
+    int now = 0;
+    CC(label, -1), CC(visit, -1);
+    inEdge[root] = 0;
+    REP(i, 0, n) {
+      ans += inEdge[i];
+      int v = i;
+      while (visit[v] != i && label[v] == -1 && v != root) {
+        visit[v] = i;
+        v = pre[v];
+      }
+      if (v != root && label[v] == -1) {
+        for (int u = pre[v]; u != v; u = pre[u]) label[u] = now;
+        label[v] = now++;
+      }
+    }
+    if (now == 0) break;
+    REP(i, 0, n) if (label[i] == -1) label[i] = now++;
+    REP(i, 0, m) {
+      int v = edge[i].v;
+      edge[i].v = label[edge[i].v];
+      edge[i].u = label[edge[i].u];
+      if (edge[i].v != edge[i].u) edge[i].c -= inEdge[v];
+    }
+    root = label[root];
+    n = now;
+  }
+  return ans;
 }
- 
+
--- graph/spanningTree/bestRadioST.cpp
+++ graph/spanningTree/bestRadioST.cpp
@@ -5,59 +5,56 @@ double mat[SIZE][SIZE], cost[SIZE][SIZE], val[SIZE][SIZE];
 int n;
 double x[SIZE], y[SIZE], height[SIZE];
 double prim(double low) {
-	bool check[SIZE];
-	memset(check, false, sizeof (check));
-	check[0] = true;
-	double totdis = 0, totcost = 0, dist[SIZE], minn = INF;
-	dist[0] = 0;
-	for (int i = 1; i < n; i++) dist[i] = INF;
-	int idx = 0, pree[SIZE], tmp;
-	memset(pree, 0, sizeof (pree));
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			val[i][j] = cost[i][j] - low * mat[i][j];
-	for (int i = 1; i < n; i++) {
-		minn = INF;
-		for (int j = 1; j < n; j++) {
-			if (!check[j]) {
-				if (dist[j] >  val[idx][j]) {
-					dist[j] = val[idx][j];
-					pree[j] = idx;
-				}
-				if (dist[j] < minn) {
-					minn = dist[j];
-					tmp = j;
-				}
-			}
-		}
-		totdis += mat[pree[tmp]][tmp];
-		totcost += cost[pree[tmp]][tmp];
-		check[tmp] = true;
-		idx = tmp;
-	}
-	return totcost / totdis;
+  bool check[SIZE];
+  memset(check, false, sizeof(check));
+  check[0] = true;
+  double totdis = 0, totcost = 0, dist[SIZE], minn = INF;
+  dist[0] = 0;
+  for (int i = 1; i < n; i++) dist[i] = INF;
+  int idx = 0, pree[SIZE], tmp;
+  memset(pree, 0, sizeof(pree));
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++) val[i][j] = cost[i][j] - low * mat[i][j];
+  for (int i = 1; i < n; i++) {
+    minn = INF;
+    for (int j = 1; j < n; j++) {
+      if (!check[j]) {
+        if (dist[j] > val[idx][j]) {
+          dist[j] = val[idx][j];
+          pree[j] = idx;
+        }
+        if (dist[j] < minn) {
+          minn = dist[j];
+          tmp = j;
+        }
+      }
+    }
+    totdis += mat[pree[tmp]][tmp];
+    totcost += cost[pree[tmp]][tmp];
+    check[tmp] = true;
+    idx = tmp;
+  }
+  return totcost / totdis;
 }
 double distance(int i, int j) {
-	return sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2));
+  return sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2));
 }
 int main() {
-	while (scanf("%d", &n) == 1 && n) {
-		for (int i = 0; i < n; i++)
-			scanf("%lf %lf %lf", &x[i], &y[i], &height[i]);
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++) {
-				mat[i][j] = distance(i, j);
-				cost[i][j] = fabs(height[i] - height[j]);
-			}
-		double low = 0, tmp;
-		while (true) {
-			tmp = prim(low);
-			if (fabs(low - tmp) < EXP)
-				break;
-			low = tmp;
-		}
-		printf("%.3lf\n", tmp);
-	}
-	return 0;
+  while (scanf("%d", &n) == 1 && n) {
+    for (int i = 0; i < n; i++) scanf("%lf %lf %lf", &x[i], &y[i], &height[i]);
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++) {
+        mat[i][j] = distance(i, j);
+        cost[i][j] = fabs(height[i] - height[j]);
+      }
+    double low = 0, tmp;
+    while (true) {
+      tmp = prim(low);
+      if (fabs(low - tmp) < EXP) break;
+      low = tmp;
+    }
+    printf("%.3lf\n", tmp);
+  }
+  return 0;
 }
 
